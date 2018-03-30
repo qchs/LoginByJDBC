a1c68eec24bcfb370fbc6b7035626292cb58b87a
qchs <vera_ziyu@163.com>
2018-03-30 20:08:20 +0800

    second commit, operations to mysql is done.

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,7 @@
+package LoginByJDBC.src;
+
 import java.io.FileReader;
+import java.sql.*;
 import java.util.Properties;
 
 /**
@@ -6,6 +9,9 @@ import java.util.Properties;
  */
 public class Demo {
     public static void main(String[] args) {
+        Connection conn=null;
+        Statement stmt=null;
+        ResultSet rs=null;
         try {
             //读取属性内容
             FileReader reader=new FileReader("D:\\Java\\src2\\LoginByJDBC\\src\\server.properties");
@@ -20,13 +26,62 @@ public class Demo {
 
             //注册驱动
             Class.forName(server);
+            //获取数据库连接
+            conn= DriverManager.getConnection(url,user,password);
+            System.out.println(conn);
+            //获取数据库操作对象
+            stmt=conn.createStatement();
+            //执行sql语句
+            //增删改：
+            String sql_ins="insert into userinfo values (\"cai \",\"bgsn\");";
+            int c =stmt.executeUpdate(sql_ins);
+            System.out.println(c);
 
+            String sql_del=" delete from userinfo where userName=\"cai\"";
+             int c1=stmt.executeUpdate(sql_del);
+            System.out.println(c1);
 
-
+            String sql_upd="update userinfo set userName=\"f\" where pwd=\"bgsn\"";
+            int c2 = stmt.executeUpdate(sql_upd);
+            System.out.println(c2);
+            //查询：
+            String sql="select * from userinfo";
+            rs=stmt.executeQuery(sql);
+            System.out.println(rs);
+            //处理查询结果集；
+            while (rs.next()){
+                String userName=rs.getString(1);
+                String pwd=rs.getString(2);
+                System.out.println(userName+"\t"+pwd);
+            }
 
 
         } catch (Exception e) {
             e.printStackTrace();
+        }finally {
+            if (rs!=null){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
 
